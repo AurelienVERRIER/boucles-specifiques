4fdda8248c028215bf389e5108b75f72743d8c1a
AurelienVERRIER <aurelien.verrier@gmail.com>
2022-06-28 16:00:42 +0200

    Exercise 5 finished

--- index.js
+++ index.js
@@ -35,10 +35,75 @@
 
 // 04 - Filter Even
 
-const numbers = [1,2,3,4,5,6,7,8]
+// const numbers = [1,2,3,4,5,6,7,8]
 
-const even = numbers.filter((numbers, i) => {
-  return numbers % 2 === 0
+// const even = numbers.filter((numbers, i) => {
+//   return numbers % 2 === 0
+// })
+
+// console.log(even)
+
+
+// 05 - Cakes
+
+
+const cakes = [
+	{
+		name: "cake",
+		flavor: "vanilla",
+		status: "available"
+	},
+	{
+		name: "brownie",
+		flavor: "chocolate",
+		status: "available"
+	},
+	{
+		name: "pie",
+		flavor: "strawberry",
+		status: "available"
+	},
+	{
+		name: "muffin",
+		flavor: "pistachio",
+		status: "available"
+	},
+	{
+		name: "donut",
+		flavor: "chocolate",
+		status: "available"
+	},
+]
+
+console.log(cakes)
+
+const chocolates = cakes.filter((cakes, i) => {
+  return cakes.flavor === "chocolate"
+  }
+)
+
+console.log(chocolates)
+
+
+const soldOut = chocolates.map((chocolate, i) => {
+  
+  chocolate.status = "Sold Out"
+  return chocolate
+
+  // return {
+  //   name: chocolate.name,
+  //   flavor: chocolate.flavor,
+  //   status: "Sold Out"
+//   }
 })
 
-console.log(even)
+
+console.log(soldOut)
+
+// const array = [1, "toto", 34, "javascript", 8]
+
+// const numbers = array.filter((array, i) => {
+//   return Number(array)
+//   // Ou return parseInt(array)
+// })
+// console.log(numbers)
