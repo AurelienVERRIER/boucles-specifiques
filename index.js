9089e146de1a2c38264ec6a7b1ca7a589748d7b9
AurelienVERRIER <aurelien.verrier@gmail.com>
2022-06-28 21:29:30 +0200

    some things done but not finished

--- index.js
+++ index.js
@@ -47,63 +47,144 @@
 // 05 - Cakes
 
 
-const cakes = [
-	{
-		name: "cake",
-		flavor: "vanilla",
-		status: "available"
-	},
-	{
-		name: "brownie",
-		flavor: "chocolate",
-		status: "available"
-	},
-	{
-		name: "pie",
-		flavor: "strawberry",
-		status: "available"
-	},
-	{
-		name: "muffin",
-		flavor: "pistachio",
-		status: "available"
-	},
-	{
-		name: "donut",
-		flavor: "chocolate",
-		status: "available"
-	},
-]
+// const cakes = [
+// 	{
+// 		name: "cake",
+// 		flavor: "vanilla",
+// 		status: "available"
+// 	},
+// 	{
+// 		name: "brownie",
+// 		flavor: "chocolate",
+// 		status: "available"
+// 	},
+// 	{
+// 		name: "pie",
+// 		flavor: "strawberry",
+// 		status: "available"
+// 	},
+// 	{
+// 		name: "muffin",
+// 		flavor: "pistachio",
+// 		status: "available"
+// 	},
+// 	{
+// 		name: "donut",
+// 		flavor: "chocolate",
+// 		status: "available"
+// 	},
+// ]
 
-console.log(cakes)
+// console.log(cakes)
 
-const chocolates = cakes.filter((cakes, i) => {
-  return cakes.flavor === "chocolate"
-  }
-)
+// const chocolates = cakes.filter((cakes, i) => {
+//   return cakes.flavor === "chocolate"
+//   }
+// )
 
-console.log(chocolates)
+// console.log(chocolates)
 
 
-const soldOut = chocolates.map((chocolate, i) => {
+// const soldOut = chocolates.map((chocolate, i) => {
   
-  chocolate.status = "Sold Out"
-  return chocolate
+//   chocolate.status = "Sold Out"
+//   return chocolate
 
   // return {
   //   name: chocolate.name,
   //   flavor: chocolate.flavor,
   //   status: "Sold Out"
   // }
+// })
+
+
+// console.log(soldOut)
+
+// // 06 - 
+
+// const pie = cakes.find((cake, i) => {
+//   return cake.name === "pie"
+// })
+
+// console.log(pie)
+
+
+// 07 - Factorielle forEach
+
+
+
+
+// listeFactiorelleDe9.forEach(listeFactiorelleDe9, i) => {
+
+// }
+
+// let number = 2
+
+// let expo = 9
+
+
+// const numbersList = expo.forEach((numbers, i) => {
+//   numbers = [i+1]
+//   console.log(numbersList)
+// })
+
+// const factiorelleDe9 = numbers.forEach((number, i) => {
+//   number = number * number
+// console.log(number)
+// })
+
+// console.log(factiorelleDe9)
+
+// [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+
+// 08 - Chess board
+
+
+const ligneImpaire =["b", "w", "b", "w", "b", "w", "b", "w"]
+ 
+
+// const plateauImpair = ligneImpaire.forEach((ligneImpaire, i) => {
+const plateauImpair = ligneImpaire.map((ligneImpaire, i) => {
+  if (ligneImpaire === "b") {
+    return ligneImpaire = `[ ]`
+  } else {
+    return ligneImpaire = `[x]`
+  }
 })
 
+console.log(plateauImpair)
 
-console.log(soldOut)
 
-// 06 - 
+const lignePaire =["w", "b", "w", "b", "w", "b", "w", "b"]
 
-const pie = cakes.find((cake, i) => {
-  return cake.name === "pie"
+const plateauPair = lignePaire.map((lignePaire, i) => {
+  if (lignePaire === "b") {
+    return lignePaire = `[ ]`
+  } else {
+    return lignePaire = `[x]`
+  }
 })
 
-console.log(pie)
+console.log(plateauPair)
+
+
+// const plateauImpair = ligneImpaire.map((letter, i) => {
+//   letter = ["X"]
+// })
+// console.log(plateauImpair)
+
+
+// 09 - Exercice 3 bis
+
+
+// const tableauOrigine = [12, 55, "hello", true, { isStudent: false }, 3]
+
+// const filtre = tableauOrigine.filter((tableauOrigine, i) => {
+//   return parseInt(tableauOrigine)    
+//   })
+
+//   console.log(filtre)
+
+  
