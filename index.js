7de0d7dac2809aa7ccae85bcc09fbe5ce933c500
AurelienVERRIER <aurelien.verrier@gmail.com>
2022-06-28 14:02:43 +0200

    2nd exercise done

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 // 01 - Map Double
 
-const array = [1, 2, 3, 4, 5]
+// const array = [1, 2, 3, 4, 5]
 
-const double = array.map((array, i) => {
-  return array = array * 2
+// const double = array.map((array, i) => {
+//   return array = array * 2
+// })
+
+// console.log(double)
+
+
+// 02 - Map Names
+
+const longNames = [{firstName: "Bond", lastName:"James Bond"},{firstName: "Roger", lastName: "Rabbit"}]
+
+const shortNames = longNames.map((longNames, i) => {
+  return name =`${longNames.firstName} ${longNames.lastName}`
+
+ 
 })
-
-console.log(double)
+console.log(longNames)
+console.log(shortNames)
